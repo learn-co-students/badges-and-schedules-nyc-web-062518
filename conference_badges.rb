0e8501a0ef01dff72bd770978d3b5bbcbf08dfa7
grandballoon <luke.sovern.conley@gmail.com>
2018-06-12 11:56:16 -0400

    badges and schedules

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.collect do |name|
+    "Hello, my name is #{name}."
+  end
+end
+
+def assign_rooms(array)
+      new_array = []
+  array.each_with_index do |name, index|
+    room_number = index + 1
+    greeting = "Hello, #{name}! You'll be assigned to room #{room_number}!"
+
+    new_array << greeting
+  end
+  new_array
+end
+
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |line|
+    puts line
+  end
+
+  assign_rooms(attendees).each do |greeting|
+    puts greeting  
+  end
+end
